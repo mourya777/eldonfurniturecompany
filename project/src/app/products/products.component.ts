d2fc2155ec98ce95f20d34ce1543e694863c618f
KratiBakshi <114287776+KratiBakshi@users.noreply.github.com>
2024-02-02 16:57:27 +0530

    product detail header footer

--- project/src/app/products/products.component.ts
+++ project/src/app/products/products.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from './product.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -15,18 +14,27 @@ export class ProductsComponent implements OnInit {
   count: number = 0;
   tableSize: number = 12;
   tableSizes: any = [3, 6, 9, 12];
-  @ViewChild('livingRoomLink', { static: false }) livingRoomLink: ElementRef | any;
+  selectedCategory: string = '';
+  filteredData: any[] = [];
+  bedroomValue: string = '';
+  categories = ['Living Room', 'Dining Room', 'Bedroom', 'Home Office'];
 
-  constructor(private productService: ProductService, private router: Router) { }
+  @ViewChild('livingRoomLink', { static: false }) livingRoomLink: ElementRef | any;
+  livingRoomText: any;
+  value: any;
+  productData2: any;
+  // filteredData: any[] = [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    console.log(this.livingRoomText)
     this.getProducts()
   }
   ngAfterViewInit() {
     // Access the text content of the anchor tag
-    const livingRoomText = this.livingRoomLink.nativeElement.textContent;
+    this.livingRoomText = this.livingRoomLink.nativeElement.textContent;
 
-    console.log('Living Room:', livingRoomText);
+    console.log('Living Room:', this.livingRoomText);
   }
 
   getProducts() {
@@ -36,14 +44,44 @@ export class ProductsComponent implements OnInit {
       if (data) {
         this.productData = data
 
+        this.productCategory = data[0].categories
+        console.log(this.productCategory)
+
       } else {
         this.productData = []
       }
     })
   }
 
-  send() {
-    this.router.navigateByUrl('/product-detail')
+  getValue(e: any, item: any) {
+
+    this.value = e.target.innerText
+
+    // const pathParts = item.split('>');
+    // this.bedroomValue = pathParts[1] ? pathParts[1].trim() : '';
+    this.productService.get().subscribe((data: any) => {
+      console.log(data)
+      if (data) {
+        this.productData2 = data
+        this.filterDataByCategoryPath(this.value, this.productData2)
+      }
+    });
+  }
+
+  filterDataByCategoryPath(selectedCategoryPath: string, data: any) {
+
+    this.filteredData = data.filter((item: any) => {
+      console.log(item.categories[0].path)
+      const pathParts = item.categories[0].path.split('>');
+      // this.bedroomValue = pathParts[1] ? pathParts[1].trim() : '';
+      console.log(pathParts)
+
+      pathParts.some(selectedCategoryPath)
+    }
+    );
+    console.log(this.filteredData)
+    debugger
+
   }
 
   onTableDataChange(event: any) {
